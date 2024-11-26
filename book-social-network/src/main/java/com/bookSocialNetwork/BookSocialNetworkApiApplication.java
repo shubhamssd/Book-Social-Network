936ab8c3fdb4a640d63bddad841e9df9f4709bfd
Shubham Dhanvate <shubhamssd10@gmail.com>
2024-11-26 17:53:41 +0530

    Implementing  User and Role

---
+++ book-social-network/src/main/java/com/bookSocialNetwork/BookSocialNetworkApiApplication.java
@@ -0,0 +1,13 @@
+package com.bookSocialNetwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookSocialNetworkApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookSocialNetworkApiApplication.class, args);
+	}
+
+}
