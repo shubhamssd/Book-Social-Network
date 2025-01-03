a6f1ad528cc5bb3f775eac4596a1583634296ce7
Shubham Shahaji Dhanvate <93417980+shubhamssd@users.noreply.github.com>
2025-01-03 18:36:12 +0530

    Update BookSocialNetworkApiApplication.java

--- book-social-network/src/main/java/com/bookSocialNetwork/BookSocialNetworkApiApplication.java
+++ book-social-network/src/main/java/com/bookSocialNetwork/BookSocialNetworkApiApplication.java
@@ -2,7 +2,12 @@ package com.bookSocialNetwork;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableAsync;
 
+@SpringBootApplication
+@EnableJpaAuditing
+@EnableAsync
 @SpringBootApplication
 public class BookSocialNetworkApiApplication {
 
