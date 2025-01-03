7d747e424d910a2e821223f35e87840149fd18cb
Shubham Shahaji Dhanvate <93417980+shubhamssd@users.noreply.github.com>
2025-01-03 18:33:14 +0530

    Update AuthenticationService.java

--- book-social-network/src/main/java/com/bookSocialNetwork/auth/AuthenticationService.java
+++ book-social-network/src/main/java/com/bookSocialNetwork/auth/AuthenticationService.java
@@ -1,11 +1,15 @@
 package com.bookSocialNetwork.auth;
 
+import com.bookSocialNetwork.email.EmailService;
+import com.bookSocialNetwork.email.EmailTemplateName;
 import com.bookSocialNetwork.role.RoleRepository;
 import com.bookSocialNetwork.user.Token;
 import com.bookSocialNetwork.user.TokenRepository;
 import com.bookSocialNetwork.user.User;
 import com.bookSocialNetwork.user.UserRepository;
+import jakarta.mail.MessagingException;
 import lombok.RequiredArgsConstructor;
+import lombok.Value;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -22,7 +26,12 @@ public class AuthenticationService {
     private final PasswordEncoder passwordEncoder;
     private final UserRepository userRepository;
     private final TokenRepository tokenRepository;
-    public void register(RegistrationRequest request) {
+
+     @Value("${application.mailing.frontend.activation-url}")
+    private String activationUrl;
+
+    
+    public void register(RegistrationRequest request) throws MessagingException {
         var userRole = roleRepository.findByName("USER")
                 //todo - better exception Handling ( It is not better way to handle exception)
                 .orElseThrow(() -> new IllegalStateException("ROLE USER was not initialized"));
@@ -43,6 +52,15 @@ public class AuthenticationService {
 
     private void sendValidationEmail(User user) {
         var newToken = generateAndSendActivationToken(user);
+         //send email
+        emailService.sendEmail(
+                user.getEmail(), 
+                user.fullName(),
+                EmailTemplateName.ACTIVATE_ACCOUNT,
+                activationUrl,
+                newToken,
+                "Account activation"
+        );
     }
 
     private String generateAndSendActivationToken(User user) {
