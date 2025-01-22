ebcd4cf59f775230b923e7af40f9b3070c80ebb9
Shubham Shahaji Dhanvate <93417980+shubhamssd@users.noreply.github.com>
2025-01-22 12:26:34 +0530

    Book Entity

---
+++ book-social-network/src/main/java/com/bookSocialNetwork/book/Book.java
@@ -0,0 +1,39 @@
+package com.bookSocialNetwork.book;
+
+import jakarta.persistence.*;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+public class Book {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+    private String tittle;
+    private String authorName;
+    private String isbn;
+    private String synopsis;
+    private String bookCover;
+    private boolean archived;
+    private boolean shareable;
+
+    @CreatedDate
+    @Column(nullable = false,updatable = false)
+    private LocalDateTime createdDate;
+    @LastModifiedDate
+    @Column(insertable = false)
+    private LocalDateTime lastModifiedDate;
+    @CreatedBy
+    @Column(nullable = false, updatable = false)
+    private Integer createdBy;
+    @LastModifiedBy
+    @Column(insertable = false)
+    private Integer lastModifiedBy;
+}
