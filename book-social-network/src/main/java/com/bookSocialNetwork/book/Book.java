a006bb546967a877a30078e5599703686e2556fd
Shubham Shahaji Dhanvate <93417980+shubhamssd@users.noreply.github.com>
2025-01-22 15:30:57 +0530

    Update Book.java
    Add relationship between entities
    
    1.Book
    2. FeedBack

--- book-social-network/src/main/java/com/bookSocialNetwork/book/Book.java
+++ book-social-network/src/main/java/com/bookSocialNetwork/book/Book.java
@@ -1,12 +1,16 @@
 package com.bookSocialNetwork.book;
 
 import com.bookSocialNetwork.common.BaseEntity;
+import com.bookSocialNetwork.feedBack.FeedBack;
+import com.bookSocialNetwork.user.User;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.experimental.SuperBuilder;
+
+import java.util.List;
 
 @Getter
 @Setter
@@ -23,6 +27,13 @@ public class Book extends BaseEntity {
     private String bookCover;
     private boolean archived;
     private boolean shareable;
+
+    @ManyToOne
+    @JoinColumn(name = "owner_id")
+    private User owner;
+
+    @OneToMany(mappedBy = "book")
+    private List<FeedBack> feedbacks;
 
 
 }
