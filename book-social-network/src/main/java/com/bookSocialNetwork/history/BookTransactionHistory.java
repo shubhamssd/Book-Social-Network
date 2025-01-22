fca002a9e5949000bfd9742071c4a0a51df0d396
Shubham Shahaji Dhanvate <93417980+shubhamssd@users.noreply.github.com>
2025-01-22 15:10:14 +0530

    Creates BookTransactioHistory Entity

---
+++ book-social-network/src/main/java/com/bookSocialNetwork/history/BookTransactionHistory.java
@@ -0,0 +1,23 @@
+package com.bookSocialNetwork.history;
+
+import com.bookSocialNetwork.common.BaseEntity;
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@Setter
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class BookTransactionHistory extends BaseEntity {
+
+
+
+    private boolean returned;
+    private boolean returnApproved;
+}
