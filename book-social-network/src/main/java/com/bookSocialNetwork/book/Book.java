f32cfaa1c723beb65efed179d7311e7e67fce0ec
Shubham Shahaji Dhanvate <93417980+shubhamssd@users.noreply.github.com>
2025-01-22 12:51:49 +0530

    Update Book.java
    Use Inheritance and refactor the code
    Created the BaseEntity class to avoid redundency.
    Modified Book class and FeedBack class .
    Created SuperClass BaseEntity and Book and Feedback class extends BaseEntity

--- book-social-network/src/main/java/com/bookSocialNetwork/book/Book.java
+++ book-social-network/src/main/java/com/bookSocialNetwork/book/Book.java
@@ -1,21 +1,21 @@
 package com.bookSocialNetwork.book;
 
+import com.bookSocialNetwork.common.BaseEntity;
 import jakarta.persistence.*;
-import org.springframework.data.annotation.CreatedBy;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedBy;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
 
-import java.time.LocalDateTime;
-
+@Getter
+@Setter
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
-@EntityListeners(AuditingEntityListener.class)
-public class Book {
+public class Book extends BaseEntity {
 
-    @Id
-    @GeneratedValue
-    private Integer id;
     private String tittle;
     private String authorName;
     private String isbn;
@@ -24,16 +24,5 @@ public class Book {
     private boolean archived;
     private boolean shareable;
 
-    @CreatedDate
-    @Column(nullable = false,updatable = false)
-    private LocalDateTime createdDate;
-    @LastModifiedDate
-    @Column(insertable = false)
-    private LocalDateTime lastModifiedDate;
-    @CreatedBy
-    @Column(nullable = false, updatable = false)
-    private Integer createdBy;
-    @LastModifiedBy
-    @Column(insertable = false)
-    private Integer lastModifiedBy;
+
 }
