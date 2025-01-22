1f651f6d829bec128fed12fe23be752394451619
Shubham Shahaji Dhanvate <93417980+shubhamssd@users.noreply.github.com>
2025-01-22 12:30:57 +0530

    Creates the Feedback Entity

---
+++ book-social-network/src/main/java/com/bookSocialNetwork/feedBack/FeedBack.java
@@ -0,0 +1,34 @@
+package com.bookSocialNetwork.feedBack;
+
+import jakarta.persistence.*;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+public class FeedBack {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+    private Double note;
+    private String comment;
+
+    @CreatedDate
+    @Column(nullable = false,updatable = false)
+    private LocalDateTime createdDate;
+    @LastModifiedDate
+    @Column(insertable = false)
+    private LocalDateTime lastModifiedDate;
+    @CreatedBy
+    @Column(nullable = false, updatable = false)
+    private Integer createdBy;
+    @LastModifiedBy
+    @Column(insertable = false)
+    private Integer lastModifiedBy;
+}
