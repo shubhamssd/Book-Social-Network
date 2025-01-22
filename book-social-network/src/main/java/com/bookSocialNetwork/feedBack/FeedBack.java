fba7d8999dfd5ae6154553818a7a49c7deb250b9
Shubham Shahaji Dhanvate <93417980+shubhamssd@users.noreply.github.com>
2025-01-22 15:31:23 +0530

    Update FeedBack.java
    Add relationship between entities
    
    1.FeedBack
    2. Book

--- book-social-network/src/main/java/com/bookSocialNetwork/feedBack/FeedBack.java
+++ book-social-network/src/main/java/com/bookSocialNetwork/feedBack/FeedBack.java
@@ -1,5 +1,6 @@
 package com.bookSocialNetwork.feedBack;
 
+import com.bookSocialNetwork.book.Book;
 import com.bookSocialNetwork.common.BaseEntity;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
@@ -20,6 +21,10 @@ public class FeedBack extends BaseEntity {
 
     private Double note;
     private String comment;
+
+    @ManyToOne()
+    @JoinColumn(name = "book_id")
+    private Book book;
 
 
 }
