0badc1dbf9618d683471618f79fcca33dd8c4e10
Shubham Shahaji Dhanvate <93417980+shubhamssd@users.noreply.github.com>
2025-01-23 17:48:13 +0530

    AuditorAware BeansConfig.java
     @Bean
        public AuditorAware<Integer> auditorAware(){
            return new ApplicationAuditAware();
        }

--- book-social-network/src/main/java/com/bookSocialNetwork/config/BeansConfig.java
+++ book-social-network/src/main/java/com/bookSocialNetwork/config/BeansConfig.java
@@ -3,8 +3,11 @@ package com.bookSocialNetwork.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -29,6 +32,12 @@ public class BeansConfig {
         return config.getAuthenticationManager();
 
     }
+
+    @Bean
+    public AuditorAware<Integer> auditorAware(){
+        return new ApplicationAuditAware();
+    }
+
 
     @Bean
     public PasswordEncoder passwordEncoder() {
