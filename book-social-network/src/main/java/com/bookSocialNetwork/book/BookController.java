a3a76741a4eb240635494e6d0ad970aa25419c42
Shubham Shahaji Dhanvate <93417980+shubhamssd@users.noreply.github.com>
2025-01-25 20:55:21 +0530

    Implement find all displayable books
       @GetMapping
        public ResponseEntity<PageResponse<BookResponse>> findAllBooks(
                @RequestParam(name = "page", defaultValue = "0",required = false) int page,
                @RequestParam(name = "size", defaultValue = "10", required = false) int size,
                Authentication connectedUser
        ){
            return ResponseEntity.ok(service.findAllBooks(page, size, connectedUser));
        }

--- book-social-network/src/main/java/com/bookSocialNetwork/book/BookController.java
+++ book-social-network/src/main/java/com/bookSocialNetwork/book/BookController.java
@@ -1,5 +1,6 @@
 package com.bookSocialNetwork.book;
 
+import com.bookSocialNetwork.common.PageResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -33,7 +34,14 @@ public class BookController {
         return ResponseEntity.ok(service.findById(bookId));
     }
 
-    
+     @GetMapping
+    public ResponseEntity<PageResponse<BookResponse>> findAllBooks(
+            @RequestParam(name = "page", defaultValue = "0",required = false) int page,
+            @RequestParam(name = "size", defaultValue = "10", required = false) int size,
+            Authentication connectedUser
+    ){
+        return ResponseEntity.ok(service.findAllBooks(page, size, connectedUser));
+    }
 
 
 }
