383258ace3b2f7f38710234738a8b6c4d10fa411
Shubham Shahaji Dhanvate <93417980+shubhamssd@users.noreply.github.com>
2025-01-25 20:57:10 +0530

    Implement find all displayable books
       public PageResponse<BookResponse> findAllBooks(int page, int size, Authentication connectedUser) {
            User user = ((User) connectedUser.getPrincipal());
            Pageable pageable = PageRequest.of(page,size, Sort.by("createdDate").descending());
            Page<Book> books = bookRepository.findAllDisplayableBooks(pageable,user.getId());
            List<BookResponse> bookResponses = books.stream()
                    .map(bookMapper :: toBookResponse)
                    .toList();
            return new PageResponse<>(
                    bookResponses,
                    books.getNumber(),
                    books.getSize(),
                    books.getTotalElements(),
                    books.getTotalPages(),
                    books.isFirst(),
                    books.isLast()
            );
        }

--- book-social-network/src/main/java/com/bookSocialNetwork/book/BookService.java
+++ book-social-network/src/main/java/com/bookSocialNetwork/book/BookService.java
@@ -1,10 +1,17 @@
 package com.bookSocialNetwork.book;
 
-import jakarta.persistence.EntityNotFoundException;
+import com.bookSocialNetwork.common.PageResponse;
 import com.bookSocialNetwork.user.User;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -20,11 +27,27 @@ public class BookService {
        return bookRepository.save(book).getId();
     }
 
-   public BookResponse findById(Integer bookId) {
+    public BookResponse findById(Integer bookId) {
         return bookRepository.findById(bookId)
                 .map(bookMapper::toBookResponse)
                 .orElseThrow(() -> new EntityNotFoundException("No book found with the ID::" + bookId));
     }
 
-  
+    public PageResponse<BookResponse> findAllBooks(int page, int size, Authentication connectedUser) {
+        User user = ((User) connectedUser.getPrincipal());
+        Pageable pageable = PageRequest.of(page,size, Sort.by("createdDate").descending());
+        Page<Book> books = bookRepository.findAllDisplayableBooks(pageable,user.getId());
+        List<BookResponse> bookResponses = books.stream()
+                .map(bookMapper :: toBookResponse)
+                .toList();
+        return new PageResponse<>(
+                bookResponses,
+                books.getNumber(),
+                books.getSize(),
+                books.getTotalElements(),
+                books.getTotalPages(),
+                books.isFirst(),
+                books.isLast()
+        );
+    }
 }
