26ee4a7429a2aafeb01164129131bfc9a4146195
Shubham Shahaji Dhanvate <93417980+shubhamssd@users.noreply.github.com>
2025-01-31 15:42:03 +0530

    Update BookRepository.java

--- book-social-network/src/main/java/com/bookSocialNetwork/book/BookRepository.java
+++ book-social-network/src/main/java/com/bookSocialNetwork/book/BookRepository.java
@@ -1,8 +1,21 @@
 package com.bookSocialNetwork.book;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface BookRepository extends JpaRepository<Book, Integer> {
+public interface BookRepository extends JpaRepository<Book, Integer> , JpaSpecificationExecutor<Book> {
+   @Query("""
+           SELECT book
+           FROM BOOK book
+           WHERE book.archived = false
+           AND book.shareable = true
+           AND book.owner.id != :userId
+           """
+   )
+    Page<Book> findAllDisplayableBooks(Pageable pageable, Integer userId);
 }
