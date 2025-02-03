2a5900281a78aa8dc2e19794a44bae758780d8b4
Shubham Shahaji Dhanvate <93417980+shubhamssd@users.noreply.github.com>
2025-02-03 18:23:35 +0530

    Update BookService.java   updateArchivedStatus
    public Integer updateArchivedStatus(Integer bookId, Authentication connectedUser) {
            Book book = bookRepository.findById(bookId)
                    .orElseThrow(() -> new EntityNotFoundException("No book found with the ID :: " + bookId));
            User user = ((User) connectedUser.getPrincipal());
            if(!Objects.equals(book.getOwner().getId(),user.getId())){
                throw new OperationNotPermittedException("You cannot update others book archived status");
            }
            book.setArchived(!book.isArchived());
            bookRepository.save(book);
            return bookId;
        }

--- book-social-network/src/main/java/com/bookSocialNetwork/book/BookService.java
+++ book-social-network/src/main/java/com/bookSocialNetwork/book/BookService.java
@@ -116,10 +116,22 @@ public class BookService {
         Book book = bookRepository.findById(bookId)
                 .orElseThrow(() -> new EntityNotFoundException("No book found with the ID :: " + bookId));
         User user = ((User) connectedUser.getPrincipal());
-        if(!Objects.equals(book.getOwner().getBooks(),user.getId())){
+        if(!Objects.equals(book.getOwner().getId(),user.getId())){
             throw new OperationNotPermittedException("You cannot update book shareable status");
         }
         book.setShareable(!book.isShareable());
+        bookRepository.save(book);
+        return bookId;
+    }
+
+  public Integer updateArchivedStatus(Integer bookId, Authentication connectedUser) {
+        Book book = bookRepository.findById(bookId)
+                .orElseThrow(() -> new EntityNotFoundException("No book found with the ID :: " + bookId));
+        User user = ((User) connectedUser.getPrincipal());
+        if(!Objects.equals(book.getOwner().getId(),user.getId())){
+            throw new OperationNotPermittedException("You cannot update others book archived status");
+        }
+        book.setArchived(!book.isArchived());
         bookRepository.save(book);
         return bookId;
     }
