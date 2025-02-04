0b83e2e30186b8ffb15f3a2c388b3a2c588c419a
Shubham Shahaji Dhanvate <93417980+shubhamssd@users.noreply.github.com>
2025-02-04 16:07:47 +0530

    Update BookController.java Implement BorrowBook feature
     @PostMapping("/borrow/{book-id}")
        public ResponseEntity<Integer> borrowBook(
                @PathVariable("book-id") Integer bookId,
                Authentication connectedUer
        ){
            return ResponseEntity.ok(service.borrowBook(bookId,connectedUer));
        }

--- book-social-network/src/main/java/com/bookSocialNetwork/book/BookController.java
+++ book-social-network/src/main/java/com/bookSocialNetwork/book/BookController.java
@@ -83,5 +83,13 @@ public class BookController {
         return ResponseEntity.ok(service.updateArchivedStatus(bookId,connectedUser));
     }
 
+     @PostMapping("/borrow/{book-id}")
+    public ResponseEntity<Integer> borrowBook(
+            @PathVariable("book-id") Integer bookId,
+            Authentication connectedUer
+    ){
+        return ResponseEntity.ok(service.borrowBook(bookId,connectedUer));
+    }
+
 
 }
