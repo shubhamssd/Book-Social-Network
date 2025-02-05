e3207e86032e7fe4754353172df81d1234fbcb8b
Shubham Shahaji Dhanvate <93417980+shubhamssd@users.noreply.github.com>
2025-02-05 10:22:34 +0530

    Implement return borrowed book
       @Query("""
               SELECT transaction
               FROM BookTransactionHistory transaction
               WHERE transaction.user.id = :userId
               AND transaction.book.id = :bookId
               AND transaction.returned = false
               AND transaction.returnedApproved = false
               """)
       Optional<BookTransactionHistory> findByBookIdAndUserId(Integer bookId, Integer userId);

--- book-social-network/src/main/java/com/bookSocialNetwork/history/BookTransactionHistoryRepository.java
+++ book-social-network/src/main/java/com/bookSocialNetwork/history/BookTransactionHistoryRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import java.util.Optional;
 
 public interface BookTransactionHistoryRepository extends JpaRepository<BookTransactionHistory,Integer> {
    @Query(
@@ -33,4 +34,14 @@ public interface BookTransactionHistoryRepository extends JpaRepository<BookTran
            AND bookTransactionHistory.returnApproved = false
            """)
     boolean isAlreadyBorrowedByUser(Integer bookId, Integer userId);
+
+   @Query("""
+           SELECT transaction
+           FROM BookTransactionHistory transaction
+           WHERE transaction.user.id = :userId
+           AND transaction.book.id = :bookId
+           AND transaction.returned = false
+           AND transaction.returnedApproved = false
+           """)
+   Optional<BookTransactionHistory> findByBookIdAndUserId(Integer bookId, Integer userId);
 }
