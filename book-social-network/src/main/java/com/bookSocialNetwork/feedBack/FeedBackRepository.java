3a14c2ca5d38646e6677a21ec747a6413fc79bdf
Shubham Shahaji Dhanvate <93417980+shubhamssd@users.noreply.github.com>
2025-02-07 19:32:35 +0530

    Implement findAllFeedbacksByBook
    public interface FeedBackRepository extends JpaRepository<FeedBack,Integer> {
    
        @Query("""
                SELECT feedback
                FROM Feedback  feedback
                WHERE feedback.book.id = :bookId
    """)
        Page<FeedBack> findAllByBookId(@Param("bookId") Integer bookId, Pageable pageable);
    }

--- book-social-network/src/main/java/com/bookSocialNetwork/feedBack/FeedBackRepository.java
+++ book-social-network/src/main/java/com/bookSocialNetwork/feedBack/FeedBackRepository.java
@@ -1,6 +1,17 @@
 package com.bookSocialNetwork.feedBack;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface FeedBackRepository extends JpaRepository<FeedBack,Integer> {
+
+    @Query("""
+            SELECT feedback
+            FROM Feedback  feedback
+            WHERE feedback.book.id = :bookId
+""")
+    Page<FeedBack> findAllByBookId(@Param("bookId") Integer bookId, Pageable pageable);
 }
